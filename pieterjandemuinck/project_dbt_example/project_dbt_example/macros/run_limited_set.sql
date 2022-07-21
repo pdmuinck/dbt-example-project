3a6e20dc727626f38f0146707d1829f53a6a5fe7
pieterjandemuinck <pieterjan.demuinck@persgroep.net>
2022-07-21 13:42:34 +0200

    test

--- pieterjandemuinck/project_dbt_example/project_dbt_example/macros/run_limited_set.sql
+++
@@ -1,7 +0,0 @@
-{% macro run_limited_set(query) %}
-    {% set limited_query %}
-        {{ query }} limit 20;
-    {% endset %}
-  {% set results = run_query(limited_query) %}
-    {% do results.print_html() %}
-{% endmacro %}
